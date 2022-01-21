3ae4a02b53bc43644ce29057181ab65e8864d751
iamidlek <iamidlek@naver.com>
2022-01-21 16:15:15 +0900

    feat: create prototype GNB - #3

--- src/common/typography.ts
+++ src/common/typography.ts
@@ -3,9 +3,7 @@ import styled from "styled-components";
 export const HeadLine2 = styled.h2`
   font-style: Bold;
   font-size: 58px;
-  @media ${(props) => props.theme.breakpoints.lg} {
-    // 만들어 보다가 폰트 사이즈를 브라우저 차에 따라 조정해야 할 경우 작성
-  }
+
   @media ${(props) => props.theme.breakpoints.md} {
   }
   @media ${(props) => props.theme.breakpoints.sm} {
@@ -14,8 +12,7 @@ export const HeadLine2 = styled.h2`
 export const HeadLine3 = styled.h3`
   font-style: Bold;
   font-size: 47px;
-  @media ${(props) => props.theme.breakpoints.lg} {
-  }
+
   @media ${(props) => props.theme.breakpoints.md} {
   }
   @media ${(props) => props.theme.breakpoints.sm} {
@@ -24,9 +21,24 @@ export const HeadLine3 = styled.h3`
 
 export const HeadLine4 = styled.h4`
   font-size: 33px;
-  @media ${(props) => props.theme.breakpoints.lg} {
+
+  @media ${(props) => props.theme.breakpoints.md} {
+  }
+  @media ${(props) => props.theme.breakpoints.sm} {
+  }
+`;
+
+export const HeadLine6 = styled.h6`
+  font-size: 20px;
+  &.active {
+    color: ${(props) => props.theme.colors.point};
   }
   @media ${(props) => props.theme.breakpoints.md} {
+    font-size: 16px;
+    color: ${(props) => props.theme.colors.white};
+    a.active & {
+      color: ${(props) => props.theme.colors.gray4};
+    }
   }
   @media ${(props) => props.theme.breakpoints.sm} {
   }
