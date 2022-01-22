e323b9f6df39b0f8e6f50a02e18db2b123c2cb92
leedawnn <2g_hee@naver.com>
2022-01-22 18:29:12 +0900

    feat: Add text, search bar

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { HeadLine4 } from '../../common/typography';
 
 const Faq = () => {
-  return <div>faq</div>;
+  return (
+    <div>
+      <section>
+        <HeadLine4>궁금한 점이 있으신가요?</HeadLine4>
+        <form>
+          <AiOutlineSearch />
+          <label htmlFor='keyword'></label>
+          <input
+            type='text'
+            id='keyword'
+            placeholder='검색어를 입력하시고 엔터를 눌러주세요.'
+          />
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default Faq;
