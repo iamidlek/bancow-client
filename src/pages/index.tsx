f51a4eaf720ac7e8a6e96a017deaaf88b09bb9da
iamidlek <iamidlek@naver.com>
2022-01-24 01:06:08 +0900

    feat: add section2 to mainpage - #12

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
-import Section1 from "../components/home/Section1";
+import Section1 from "../component/home/Section1";
+import Section2 from "../component/home/Section2";
 
 const Home: NextPage = () => {
   return (
     <>
       <Section1 />
-      <div style={{ height: "1120px", border: "1px solid red" }}>어플광고</div>
+      <Section2 />
       <div style={{ height: "1694px", border: "1px solid red" }}>1구매하기</div>
       <div style={{ height: "1120px", border: "1px solid red" }}>2사육하기</div>
       <div style={{ height: "1334px", border: "1px solid red" }}>3출하하기</div>
