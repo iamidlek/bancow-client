1543712a0b15f74990c5bbbf9d11ab7b7698af14
iamidlek <iamidlek@naver.com>
2022-01-23 20:17:32 +0900

    feat: use redux state control darkmode

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,12 +1,17 @@
+import { useSelector } from "react-redux";
+import { DarkModeState } from "../redux/thememode";
 import { ThemeProvider } from "styled-components";
-import { themeDefault } from "../themes/default";
+import { darkTheme, themeDefault } from "../themes/default";
 import { GlobalStyles } from "./globals";
 
-const Theme = ({ children }: any) => (
-  <ThemeProvider theme={themeDefault}>
-    <GlobalStyles {...themeDefault} />
-    {children}
-  </ThemeProvider>
-);
+const Theme = ({ children }: any) => {
+  const isDark = useSelector((state: DarkModeState) => state.isDark);
+  return (
+    <ThemeProvider theme={isDark ? darkTheme : themeDefault}>
+      <GlobalStyles {...themeDefault} />
+      {children}
+    </ThemeProvider>
+  );
+};
 
 export default Theme;
