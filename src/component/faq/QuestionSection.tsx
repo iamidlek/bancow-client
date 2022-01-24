15d95959daef2859521f0e9f8208e2da3f0aa827
leedawnn <2g_hee@naver.com>
2022-01-24 19:02:56 +0900

    feat: Create question button components for each category

---
+++ src/component/faq/QuestionSection.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Question = () => {
+  return (
+    <h1>Question</h1>
+  )
+}
+
+export default Question;
---
+++ src/component/faq/components/QstButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from 'styled-components';
+
+// TODO : 질문 조회 api 연결
+
+const QstButton = () => {
+  return (
+    <button>Category</button>
+  )
+}
+
+export default QstButton;
