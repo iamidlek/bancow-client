fc2949a1c04fc3fa86b3b04b059d1fd9ba888419
iamidlek <iamidlek@naver.com>
2022-01-24 20:31:21 +0900

    fix: rename and import

--- src/component/story/section2.tsx
+++ src/component/story/section2.tsx
@@ -1,39 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 
-export const SectionBox = styled.div` 
+export const SectionBox = styled.div`
   box-sizing: border-box;
-  width:100%;
-  height:1916px;
-
+  width: 100%;
+  height: 1916px;
 
   @media ${(props) => props.theme.breakpoints.md} {
-    height:658px;
+    height: 658px;
   }
 
   @media ${(props) => props.theme.breakpoints.sm} {
-    height:588px;
+    height: 588px;
   }
 `;
 
-  export const Card = styled.div`
-  width:1300px;
-  height:500px;
+export const Card = styled.div`
+  width: 1300px;
+  height: 500px;
   margin: 0 auto;
-  margin-bottom:93px;
+  margin-bottom: 93px;
   border-radius: 20px;
-  box-shadow: 0px 0px 32px 0px rgba(0,0,0,0.2);
+  box-shadow: 0px 0px 32px 0px rgba(0, 0, 0, 0.2);
 
   @media ${(props) => props.theme.breakpoints.md} {
-    width:585px;  
-    height:118px;
+    width: 585px;
+    height: 118px;
   }
 
   @media ${(props) => props.theme.breakpoints.sm} {
-    width:308px;
-    height:118px;
+    width: 308px;
+    height: 118px;
   }
-  `
+`;
 const Section2 = () => {
   return (
     <SectionBox>
@@ -47,7 +46,7 @@ const Section2 = () => {
         <div>하이</div>
       </Card>
     </SectionBox>
-  )
-}
+  );
+};
 
 export default Section2;
