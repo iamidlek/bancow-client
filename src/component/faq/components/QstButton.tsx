c7c00d4bea45e133f40586a1bd1fcb52365fd6d2
leedawnn <2g_hee@naver.com>
2022-01-24 20:36:41 +0900

    style: Apply style to each category button

--- src/component/faq/components/QstButton.tsx
+++ src/component/faq/components/QstButton.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import styled from 'styled-components';
 
-// TODO : 질문 조회 api 연결
+export const CategoryBtn = styled.button`
+  border: 0.5px solid rgba(0, 75, 250, 0.5);
+  border-radius: 6px;
+  width: 63px;
+  height: 54px;
+  font-size: 23px;
+  background-color: ${(props) => props.theme.colors.white};
+
+  @media ${(props) => props.theme.breakpoints.md} {
+    border: 0.5px solid rgba(0, 75, 250, 0.5);
+    border-radius: 6px;
+    width: 28px;
+    height: 23.48px;
+    font-size: 10px;
+  }
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    border: 0.11px solid rgba(0, 75, 250, 0.5);
+    border-radius: 1.34px;
+    width: 14.04px;
+    height: 12.04px;
+    font-size: 10px;
+  }
+`
 
 const QstButton = () => {
   return (
-    <button>Category</button>
+    <CategoryBtn>btn</CategoryBtn>
   )
 }
 
