a1e032cfab0f853bde42e5b4eac15cd3dc3c4273
leedawnn <2g_hee@naver.com>
2022-01-25 14:11:11 +0900

    style: Add margin to category button

--- src/component/faq/QuestionSection.tsx
+++ src/component/faq/QuestionSection.tsx
@@ -3,19 +3,34 @@ import styled from "styled-components";
 import QstButton from "./components/QstButton";
 
 export const Wrapper = styled.div`
-  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
+
+export const QstButtonStyle = styled(QstButton)` 
+  margin-right: 15px;
+
+  @media ${(props) => props.theme.breakpoints.md} {
+    margin-right: 7px;
+  }
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    margin-right: 4px;
+  }
+`
+
+// TODO: 백오피스에서 faq crud 관리(api 호출)
 
 const Question = () => {
   return (
     <>
     <Wrapper>
-      <QstButton />
-      <QstButton />
-      <QstButton />
-      <QstButton />
-      <QstButton />
-      <QstButton />
+      <QstButtonStyle/>
+      <QstButtonStyle />
+      <QstButtonStyle />
+      <QstButtonStyle />
+      <QstButtonStyle />
     </Wrapper>
     </>
   )
