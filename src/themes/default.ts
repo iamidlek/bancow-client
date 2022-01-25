239b333ac1193ee3fa6ab4bbed3ab03ef3f846fe
iamidlek <iamidlek@naver.com>
2022-01-26 00:07:17 +0900

    feat: add darkmode theme

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -18,6 +18,16 @@ export const themeDefault: DefaultTheme = {
   breakpoints: {
     sm: "screen and (max-width: 576px)", // mobile
     md: "screen and (max-width: 1024px)" // ipad pro
+  },
+  alt: {
+    backgroud1: "#FFFFFF", // 흰 배경 => darkmode기본 배경색
+    backgroud2: "#0048FA", // point(파랑) 배경 => darkmode기본 배경색
+    backgroud3: "#F9FAFB", // sub gray 2(완전 연한 회색 거의 흰색) => 완전black 배경
+    backgroud4: "#FFFFFF", // 흰 배경 => subgray
+    backgroud5: "#FFFFFF", // 흰 배경 => point(파랑)
+    backgroud6: "#E8E8E8", //연회색 배경 => 검정색 배경
+    text1: "#000000", // 검정 글씨 => 흰 글씨
+    text2: "#4B5360" // 회색 글씨 => 흰 글씨
   }
 };
 
@@ -39,5 +49,15 @@ export const darkTheme: DefaultTheme = {
   breakpoints: {
     sm: "screen and (max-width: 576px)", // mobile
     md: "screen and (max-width: 1024px)" // ipad pro
+  },
+  alt: {
+    backgroud1: "#353639",
+    backgroud2: "#353639",
+    backgroud3: "#000000",
+    backgroud4: "#E8E8E8",
+    backgroud5: "#0048FA",
+    backgroud6: "#000000",
+    text1: "#FFFFFF",
+    text2: "#FFFFFF"
   }
 };
