ecde8f7c4ce164ae527b79bf40c68ae27f26abcd
leedawnn <2g_hee@naver.com>
2022-01-27 21:56:07 +0900

    feat: Add QuestionSection.tsx in index

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchSection from "../../component/faq/SearchSection";
-import CategorySection from '../../component/faq/CategorySection'
+import CategorySection from "../../component/faq/CategorySection"
+import QuestionSection from "../../component/faq/QuestionSection"
 
 const Faq = () => {
   return (
     <>
       <SearchSection />
       <CategorySection />
+      <QuestionSection />
     </>
   );
 };
