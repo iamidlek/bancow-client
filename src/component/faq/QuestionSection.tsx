1417e33e24750b2b93d4509c4d9ca5be2a627e4b
leedawnn <2g_hee@naver.com>
2022-01-28 23:24:34 +0900

    style: Apply style to ul, div of question part

--- src/component/faq/QuestionSection.tsx
+++ src/component/faq/QuestionSection.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 
+const Wrapper = styled.ul`
+  display: flex;
+  justify-content: center;
+  margin-top: 65px;
+`;
+
+const QuestionBox = styled.div`
+  width: 722px;
+  height: 83px;
+  border-top: 1px solid ${(props) => props.theme.colors.gray3};
+  border-bottom: 1px solid ${(props) => props.theme.colors.gray3};
+`;
+
 const Question = () => {
   return (
-    <ul>
-      
-    </ul>
-  )
-}
+    <Wrapper>
+      <QuestionBox></QuestionBox>
+    </Wrapper>
+  );
+};
 
 export default Question;
