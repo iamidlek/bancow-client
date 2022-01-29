98ce74eb1bce190ff5fe5c905600a744ab6c28cf
leedawnn <2g_hee@naver.com>
2022-01-29 23:31:22 +0900

    feat: Rendering QuestionBox component according to the number of questions

--- src/component/faq/QuestionSection.tsx
+++ src/component/faq/QuestionSection.tsx
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
-import questionData from "./data/questionData.json";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 export const Wrapper = styled.ul`
@@ -24,27 +23,67 @@ export const MdKeyboardArrowDownStyle = styled(MdKeyboardArrowDown)`
   height: 24px;
 `;
 
-// class QuetionLoad extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       questionList = []
-//     };
-//   }
-// };
 
-// componentDidMount() {
+function Questions ({ questions }) {
+  return (
+    <div>
+      <b>{questions.question}</b> <span>({questions.answer})</span>
+    </div>
+  );
+}
 
+function questionDate() {
+  const questions = [
+    {
+      "id": 1,
+      "faq_category": ["전체", "구매하기"],
+      "question": "Q. 구매 금액은 어떻게 지불하나요?",
+      "answer": "보유하고 계신 예치금에서 구매하신 지분 금액만큼 차감됩니다.",
+      "create_date": "2022-01-25",
+      "update_date": "2022-01-25"
+    },
+    {
+      "id": 2,
+      "faq_category": ["전체", "사육중"],
+      "question": "Q. 구매 금액은 어떻게 지불하나요?",
+      "answer": "보유하고 계신 예치금에서 구매하신 지분 금액만큼 차감됩니다.",
+      "create_date": "2022-01-25",
+      "update_date": "2022-01-25"
+    },
+    {
+      "id": 3,
+      "faq_category": ["전체", "사육중"],
+      "question": "Q. 구매 금액은 어떻게 지불하나요?",
+      "answer": "보유하고 계신 예치금에서 구매하신 지분 금액만큼 차감됩니다.",
+      "create_date": "2022-01-25",
+      "update_date": "2022-01-25"
+    }
+  ];
+};
+
+//   return (
+//     <div>
+//       {users.map((user) => (
+//         <User user={user} key={user.id} />
+//       ))}
+//     </div>
+//   );
 // }
 
+// export default UserList;
+
 const Question = () => {
+
   return (
     <Wrapper>
-      <QuestionBox>
+      {questions.map((qs, as, id) => (
+      <QuestionBox qs={questions.question} as={questions.answer} key={questions.id}>
         <MdKeyboardArrowDownStyle />
       </QuestionBox>
+      )}
     </Wrapper>
   );
 };
 
 export default Question;
+
