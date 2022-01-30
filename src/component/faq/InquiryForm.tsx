7977401ca2109ca3049f412ce239642322151ec6
leedawnn <2g_hee@naver.com>
2022-01-31 01:20:34 +0900

    feat: Create faq/InquiryForm.tsx

---
+++ src/component/faq/InquiryForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const InquiryForm = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default InquiryForm;
