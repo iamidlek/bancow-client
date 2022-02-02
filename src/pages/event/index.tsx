7e505e4898505e9bd266f56493619944797c5fe9
iamidlek <iamidlek@naver.com>
2022-02-02 13:22:31 +0900

    feat: notice page add sections - #49

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,26 +1,28 @@
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import EventDetail from "../../component/event/components/EventDetail";
-import Section1 from "../../component/event/Section1";
-import Section2 from "../../component/event/Section2";
-import Section3 from "../../component/event/Section3";
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Wrapper } from '../../common/typography';
+import EventDetail from '../../component/event/components/EventDetail';
+import Section1 from '../../component/event/Section1';
+import Section2 from '../../component/event/Section2';
+import Section3 from '../../component/event/Section3';
 
 const Event = () => {
-  const { query } = useRouter();
-  const [isEnd, setIsEnd] = useState(false);
-  return (
-    <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
-      <Section1 />
-      {query.eventID ? (
-        <EventDetail />
-      ) : (
-        <>
-          <Section2 isEnd={isEnd} setIsEnd={setIsEnd} />
-          <Section3 isEnd={isEnd} />
-        </>
-      )}
-    </div>
-  );
+	const { query } = useRouter();
+	const [isEnd, setIsEnd] = useState(false);
+	return (
+		<Wrapper>
+			<Section1 />
+			{query.eventID ? (
+				<EventDetail />
+			) : (
+				<>
+					<Section2 isEnd={isEnd} setIsEnd={setIsEnd} />
+					<Section3 isEnd={isEnd} />
+				</>
+			)}
+		</Wrapper>
+	);
 };
 
 export default Event;
