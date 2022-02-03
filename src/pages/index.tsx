449fbfadd1d14055f5e8a1b192b3df8a41d24787
iamidlek <iamidlek@naver.com>
2022-02-03 19:27:01 +0900

    refactor: footer separation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,32 @@
-import type { NextPage } from "next";
-import Footer from "../common/global/Footer";
-import Section0 from "../component/home/Section0";
-import Section1 from "../component/home/Section1";
-import Section2 from "../component/home/Section2";
-import Section3 from "../component/home/Section3";
-import Section4 from "../component/home/Section4";
-import Section5 from "../component/home/Section5";
-import Section6 from "../component/home/Section6";
-import Section7 from "../component/home/Section7";
-import Section8 from "../component/home/Section8";
+import type { NextPage } from 'next';
+import Footer from '../common/global/Footer';
+import SubFooter from '../common/global/SubFooter';
+import Section0 from '../component/home/Section0';
+import Section1 from '../component/home/Section1';
+import Section2 from '../component/home/Section2';
+import Section3 from '../component/home/Section3';
+import Section4 from '../component/home/Section4';
+import Section5 from '../component/home/Section5';
+import Section6 from '../component/home/Section6';
+import Section7 from '../component/home/Section7';
+import Section8 from '../component/home/Section8';
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Section0 />
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-      <Section6 />
-      <Section7 />
-      <Section8 />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Section0 />
+			<Section1 />
+			<Section2 />
+			<Section3 />
+			<Section4 />
+			<Section5 />
+			<Section6 />
+			<Section7 />
+			<Section8 />
+			<SubFooter />
+			<Footer />
+		</>
+	);
 };
 
 export default Home;
